14564c4f052018bc38e1b4b60d960fa6bd484321
Felix Huber <fhuber@conet.de>
2022-06-10 11:31:05 +0200

    Created DataRoot

---
+++ ie21a-class-championship/src/main/java/de/bkhennef/ie21a/cc/database/DataRoot.java
@@ -0,0 +1,15 @@
+package de.bkhennef.ie21a.cc.database;
+
+import java.util.List;
+import de.bkhennef.ie21a.cc.core.entities.Game;
+import de.bkhennef.ie21a.cc.core.entities.Match;
+import de.bkhennef.ie21a.cc.core.entities.Player;
+
+public class DataRoot {
+
+    private List<Player> players;
+
+    private List<Match> matches;
+
+    private List<Game> games;
+}
